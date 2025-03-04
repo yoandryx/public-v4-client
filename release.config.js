ba99b8a2ac3743f7cd233312f192c9f429aaa313
sean-sqds <sean@sqds.io>
2025-03-04 18:15:22 -0500

    chore(ci): semver release config

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: false,
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "CHANGELOG.md"],
+                message: "chore(release): bump version to ${nextRelease.version} [skip ci]",
+            },
+        ],
+        "@semantic-release/github",
+    ],
+};
